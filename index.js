bb6474cca65eb77f6c76d512f5249071eefe7718
chijioke <superkingsely@gmail.com>
2024-03-10 13:30:41 -0700

    scroll added

--- index.js
+++ index.js
@@ -24,3 +24,14 @@ const typed=new Typed('.hero-con span',{
     backSpeed:100,
     backDelay:1500,
 })
+// -------------scrollreveal
+ScrollReveal({
+    reset:true,
+    distance:'100px',
+    duration:2000,
+    delay:200
+});
+ScrollReveal().reveal('.one-left',{origin:'left'})
+ScrollReveal().reveal('.three-left',{origin:'left'})
+ScrollReveal().reveal('.two-right',{origin:'right'})
+ScrollReveal().reveal('.four-right',{origin:'right'})
