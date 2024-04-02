43cbd6358c491d70177d125015413d4d95be5825
chijioke <superkingsely@gmail.com>
2024-04-01 18:12:45 -0700

    okay

---
+++ contact.js
@@ -0,0 +1,55 @@
+const form=document.querySelector('form')
+const email=document.getElementById('email')
+const firstName=document.getElementById('firstName')
+const lastName=document.getElementById('lastName')
+const message=document.getElementById('message')
+
+
+
+
+const sendEmail=()=>{
+    let bodyMessage=`FullName: ${firstName.value} ${lastName.value} <br> Email:${email.value} <br> Message: ${message.value}`
+    const validation=()=>{
+        if(firstName.value!=''&& lastName.value!='' && message.value!=''){
+            console.log('better')
+           
+            return "superkingsely@gmail.com"
+        }else{
+            console.log('not good')
+            return ''
+            // return  '<h1>bad</h1>'
+        }
+    }
+    Email.send({
+        SecureToken : "f978e73c-127d-47e2-887c-1a709c11f1a2",
+        To : 'dev.drivelink@gmail.com',
+        From : validation(),
+        Subject : "Please Attention here A client is trying to contact DriveLink",
+        Body : bodyMessage
+    }).then(
+        message => {
+            console.log(message)
+            // alert(message)
+            if( message=='OK'){
+            Swal.fire({
+                title: "Success!",
+                text: "Message Sent Successfully!",
+                icon: "success"
+              });
+        }else{
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Something went wrong!",
+                // footer: '<a href="#">Why do I have this issue?</a>'
+              });
+        }
+      }
+    );
+
+}
+form.onsubmit=(e)=>{
+    e.preventDefault();
+    console.log('okay')
+    sendEmail();
+}
